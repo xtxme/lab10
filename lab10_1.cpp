bf08c0a79998264c8afdf9c81a845f476a131dc1
Tanapron Tangpadungsuk <tt.icy013@gmail.com>
2023-12-19 15:21:58 +0700

    lab10.2 Complete

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -3,12 +3,13 @@ using namespace std;
 
 int main(){
 	int count[5] = {}; //Declare array count for counting A,B,C,D,F and initialize all element = 0
+	char grade,A,B,C,D,F;
 	cout << "Please input grade of each student (A-F) or input 0 to exit.";
 	do{
 		cout << "Student [" << "]: ";
 		cin >> grade; //The loop must be terminated when grade = '0'
-		if(true) // if grade is A
-			//Do something
+		if(grade == A) // if grade is A
+			count [1] =+ 1
 		}else if(true) // if grade is B
 			//Do something
 		//and so on ... for grade = C, D, F	
--- lab10_2.cpp
+++ lab10_2.cpp
@@ -6,13 +6,19 @@ using namespace std;
 int main (){
 	ifstream source;
 	ofstream dest;
-	source.open("cheerbook.txt") ;
+	source.open("cheerbook.txt");
 	dest.open("cheerbook_copy.txt");
 	
-	//กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
-	//โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
+	string textline;
+	dest << "-------------------- BOOM ---------------------\n";
+	while (getline(source, textline)){
+	cout << textline << '\n';
+	}
+	
+	dest << "-------------------- HA!! ---------------------\n";
 
     source.close();
     dest.close();
 	return 0;
 }
+

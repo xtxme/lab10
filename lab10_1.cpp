05b0c4993a5550679239448d438d2cbf294fed64
Tanapron Tangpadungsuk <tt.icy013@gmail.com>
2023-12-22 11:34:44 +0700

    lab10.1 Complete

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -2,35 +2,44 @@
 using namespace std;
 
 int main(){
-	int count[5] = {0,0,0,0,0}; //Declare array count for counting A,B,C,D,F and initialize all element = 0
-	char A,B,C,D,F;
-	string grade;
-	cout << "Please input grade of each student (A-F) or input 0 to exit.";
-	do{
-		cout << "Student [" << "]: ";
-		cin >> grade; //The loop must be terminated when grade = '0'
-		if(grade == A) // if grade is A
-		{
-			count [1] += 1;
-		}	
-		if (grade == B)
-		{
-			count [2] += 1;
-		}
-		
-		}else if(true) // if grade is B
-			//Do something
-		//and so on ... for grade = C, D, F	
-		}else{ // grade is wrong input
-			//Do something
-		} 
-	}while(true);
-	
-	
-	cout << "In total ? students.";
-	cout << "A = " << count[0] <<", ";
-	cout << "B = " << count[1] <<", ";	
-	//	and so on ... for grade = C, D, F	
-	
-	return 0;
+    
+    int count[5] = {0,0,0,0,0}; //Declare array count for counting A,B,C,D,F and initialize all element = 0
+	char grade;
+	int student = 1;
+    cout << "Please input grade of each student (A-F) or input 0 to exit.\n";
+    do{
+        cout << "Student ["  << student <<  "]: ";
+        cin >> grade; //The loop must be terminated when grade = '0'
+        if(grade=='A') {// if grade is A
+            count[0]+=1 ;
+            student+=1 ;
+        }else if(grade=='B'){ // if grade is B
+            count[1]+=1;
+            student+=1 ;
+        }else if(grade=='C'){ // if grade is B
+            count[2]+=1 ;
+            student+=1 ;
+        }else if(grade=='D'){ // if grade is B
+            count[3]+=1 ;
+            student+=1 ;
+        }else if(grade=='F'){ // if grade is B
+            count[4]+=1 ;
+            student+=1 ;
+        //and so on ... for grade = C, D, F
+        }else if(grade == '0'){ break;
+        }else cout << "Wrong input. Please input again.\n" ;
+
+    }while(true);
+
+
+    cout << "In total " << student-1 << " students.\n";
+    cout << "A = " << count[0] <<", ";
+    cout << "B = " << count[1] <<", ";
+    cout << "C = " << count[2] <<", ";
+    cout << "D = " << count[3] <<", ";
+    cout << "F = " << count[4] ;
+
+    //    and so on ... for grade = C, D, F
+
+    return 0;
 }
